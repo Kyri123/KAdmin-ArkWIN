ad4c822c04502761cb87691bd72e4ab3a9fdcbb9
Kyrium <olli143@live.de>
2020-11-14 16:39:08 +0100

    Update backgroundRunner.js

--- Panel/app/src/background/backgroundRunner.js
+++ Panel/app/src/background/backgroundRunner.js
@@ -218,6 +218,7 @@ module.exports = {
      * Prüft nach neuer Panel Version
      */
     backgroundUpdater: async () => {
+        global.checkIsRunning = undefined;
         async function backgroundUpdater() {
             var options = {
                 url: `https://api.github.com/repos/Kyri123/ArkAdminWin/branches/${PANEL_MAIN.panelBranch}`,
@@ -232,7 +233,6 @@ module.exports = {
                     if(debug) console.log('\x1b[33m%s\x1b[0m', `[${dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")}] Auto-Updater: \x1b[91m${PANEL_LANG.updaterLOG.conErr}`);
                 } else if (res.statusCode === 200) {
                     // Prüfe SHA mit API
-                    global.checkIsRunning = undefined;
                     fs.readFile("./app/data/sha.txt", 'utf8', (err, data) => {
                         if (err === null) {
                             if (data === api.commit.sha) {
