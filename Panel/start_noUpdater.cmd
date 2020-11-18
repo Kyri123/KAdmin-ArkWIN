aa2a8f0e5770c926c2c925dee194132acd6c63ed
Kyrium <olli143@live.de>
2020-11-18 16:05:05 +0100

    Update 0.0.2a

---
+++ Panel/start_noUpdater.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+WHERE npm
+IF %ERRORLEVEL% NEQ 0 ECHO Python command "npm" not found!
+IF %ERRORLEVEL% NEQ 0 pause
+IF %ERRORLEVEL% NEQ 0 exit
+
+WHERE node
+IF %ERRORLEVEL% NEQ 0 ECHO Python command "node" not found!
+IF %ERRORLEVEL% NEQ 0 pause
+IF %ERRORLEVEL% NEQ 0 exit
+
+echo -----------------------------------------
+echo        Update Module + Start
+echo -----------------------------------------
+npm i & npm update & npm fund & start "ArkAdminWIN" cmd /k "node app.js startedWithoutUpdater & start "ArkAdminWIN Starter" start_noUpdater.cmd & exit" & exit
