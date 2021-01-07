211982a4b1e92fa73939f81f2004e99aa51c46de
Kyrium <olli143@live.de>
2021-01-07 09:41:41 +0100

    Testing entfernt

--- Panel/app.js
+++ Panel/app.js
@@ -60,7 +60,6 @@ catch (e) {
   console.log('\x1b[33m%s\x1b[0m', `[${dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss")}]\x1b[31m not Installed... load Installer`);
 }
 //Installed = false; //(Testing)
-Installed = true;
 
 require('./app/main/sqlLoader.js');
 
